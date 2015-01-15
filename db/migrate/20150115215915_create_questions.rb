b64e3472a61d31d2ec411f627886abf6b97a4a58
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-15 17:06:22 -0500

    Add Question Model and Migration

---
+++ db/migrate/20150115215915_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :content
+      t.references :user, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :questions, :users
+  end
+end
