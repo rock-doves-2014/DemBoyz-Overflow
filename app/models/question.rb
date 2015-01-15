592435a23fe0c00e3b4d2488c1ef5c9c36690e84
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-15 17:17:55 -0500

    Uncommeneted text in Question Model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
-  has_many :upVotes Can we make these polymorphic?
+  has_many :upVotes
   has_many :downVotes
   has_many :comments
 end
