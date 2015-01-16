02f8b235bf94be9fb2bd180caaf94dda7e123885
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-16 13:07:37 -0500

    Welcome#login: Working action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,18 @@ class WelcomeController < ApplicationController
   end
 
   def login
+    @user = User.find(params[:id]).try(:authenticate, params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
+    end
   end
 
   def logout
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
--- spec/controllers/welcome_spec.rb
+++ spec/controllers/welcome_spec.rb
@@ -1,22 +1,38 @@
 require 'rails_helper'
 
-# describe WelcomeController do
-#   describe 'GET #login' do
-#     context '' do
-#       it ""
-#     end
-#   end
+describe WelcomeController do
 
-#   describe 'GET #logout' do
-#     context '' do
-#       it ""
-#     end
-#   end
+  describe 'GET #login' do
+    context 'with valid attributes' do
+      it "assigns the user's id to session[:user_id]" do
+        user = create(:user)
+        session[:user_id] = nil
+        post :login, id: user, password: user.password
+        expect(session[:user_id]).to eq(user.id)
+      end
+      it "redirects to the user_path" do
+        user = create(:user)
+        session[:user_id] = nil
+        post :login, id: user, password: user.password
+        expect(response).to redirect_to user_path(user)
+      end
+    end
 
-#   describe 'GET #signup' do
-#     context '' do
-#       it ""
-#     end
-#   end
+    context 'with invalid attributes' do
+      it "doesn't assign the user's id to session[:user_id]" do
+        user = create(:user)
+        session[:user_id] = nil
+        post :login, id: user, password: 'invalid_id'
+        expect(session[:user_id]).not_to eq(user.id)
+      end
+      it "redirects to the root_path" do
+        user = create(:user)
+        session[:user_id] = nil
+        post :login, id: user, password: 'invalid_id'
+        expect(response).to redirect_to root_path
+      end
+    end
 
-# end
+  end
+
+end
