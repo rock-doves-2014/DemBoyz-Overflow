2d50ae457aca818113070fb0389b437dfa22fb4b
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-15 22:14:09 -0500

    User#create: Working action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -30,4 +30,34 @@ describe UsersController do
     end
   end
 
+  describe 'POST #create' do
+
+    context "with valid attributes" do
+      it "creates a new user" do
+        expect{
+          post :create, user: attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+
+      it "redirects to the root_path" do
+        post :create, user: attributes_for(:user)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't create a new user" do
+        expect{
+          post :create, user: attributes_for(:invalid_user)
+        }.not_to change(User, :count)
+      end
+
+      it "redirects back to the new_user_path" do
+        post :create, user: attributes_for(:invalid_user)
+        expect(response).to redirect_to new_user_path
+      end
+    end
+
+  end
+
 end
