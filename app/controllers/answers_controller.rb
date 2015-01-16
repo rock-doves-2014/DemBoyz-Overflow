ba3cf63b9935d453d0386db172b1ca7e96899bfd
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-16 08:57:51 -0500

    Add answers#new and answers#create methods to Answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,21 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = Answer.new
+    @question_id = params[:question_id]
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@answer.question.id)
+    else
+      render @answer
+    end
+  end
+
+  private
+
+    def answer_params
+      params.require(:answer).permit(:content, :question_id)
+    end
 end
