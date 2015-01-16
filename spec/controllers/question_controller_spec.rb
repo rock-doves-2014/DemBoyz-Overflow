e0df96fedde970a9dbcc788028653914b5010b5a
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-15 20:36:49 -0500

    Add questions#delete test

--- spec/controllers/question_controller_spec.rb
+++ spec/controllers/question_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+ 
 describe QuestionsController do
   describe "POST create" do
     it "creates a new Question" do
@@ -8,6 +9,14 @@ describe QuestionsController do
       }.to change(Question, :count).by(1)
     end
   end
+
+
+  describe "DELETE destroy" do
+    it "destroys a requested question" do
+      question = Question.create!(title: 'Title', content: 'Content')
+      expect {
+        delete :destroy, {:id => question.to_param}
+      }.to change(Question, :count).by(-1)
+    end
+  end
 end
-
-
