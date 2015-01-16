d1d59c175c3741a19e1ca80feb3eee1471337766
fete <d1swaay@gmail.com>
2015-01-16 12:38:02 -0500

    added routes to create and display tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,33 @@
 class TagsController < ApplicationController
 
+  def index
+   @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
   end
 
   def new
     @tag = Tag.new
+  end
+
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      redirect_to tag_path(@tag)
+    else
+      render :new
+    end
+  end
+
+  
+  private
+  
+  def tag_params
+    params.require(:tag).permit(:title)
   end
 
 end
