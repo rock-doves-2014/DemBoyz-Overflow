c6c3ad82ee425bac71bea5fd2a86ba0ec466942e
Luke Kedziora <lukekedziora@gmail.com>
2015-01-17 14:21:38 -0500

    editing question model, switched .each to .map

--- app/models/question.rb
+++ app/models/question.rb
@@ -19,11 +19,7 @@ class Question < ActiveRecord::Base
   def get_question_tags
     tags = []
     qts = QuestionTag.where(question_id: self.id)
-
-    qts.each do |qt|
-      tags << Tag.find(qt.tag_id)
-    end
-
+    qts.map { |qt| tags << Tag.find(qt.tag_id) }
     tags
   end
 
