92f4f8e9f53358a8d1920b9ee43540c4c3844658
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-16 21:12:13 -0500

    Tags: Implement tags creation
    Tags can be created from new question form
    New method in the question model create_tags
    Create action from Questions controller calls method

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :question_tags
   has_many :tags, through: :question_tags
+
+  def create_tags(tags)
+    tags[:title].split(',').each do |tag|
+      self.tags.create(title: tag.strip)
+    end
+  end
 end
