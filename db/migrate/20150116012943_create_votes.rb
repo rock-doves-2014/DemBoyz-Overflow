28a830b09bb699c115f1ea7d639104d6a61ea948
Luke Kedziora <lukekedziora@gmail.com>
2015-01-16 19:50:15 -0500

    fixed typo in class Vote

--- db/migrate/20150116012943_create_votes.rb
+++ db/migrate/20150116012943_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.boolean :status
-      t.references :answergit add
+      t.references :answer
 
       t.timestamps
     end
