99bacf6ec274aa156b98a5ad8ff64ff683e02b40
Luke Kedziora <lukekedziora@gmail.com>
2015-01-17 13:20:20 -0500

    added method to extract tags from question tags object in QuestionTag model

--- app/models/question_tag.rb
+++ app/models/question_tag.rb
@@ -1,4 +1,14 @@
 class QuestionTag < ActiveRecord::Base
   belongs_to :tag
   belongs_to :question
+
+    def get_question_tags
+      tags = []
+      qts = QuestionTag.where(question_id: self.id)
+
+        qts.each do |qt|
+          tags << Tag.find(qt.tag_id)
+        end
+      tags
+    end
 end
